5b76694d1d83ac49e99376581314a70cad1998a1
ahhhlvin <alvkuang@gmail.com>
2015-08-01 14:22:35 -0400

    start from scratch

--- app/src/main/java/c4q/nyc/ramonaharrison/meshnyc/MainActivity.java
+++ app/src/main/java/c4q/nyc/ramonaharrison/meshnyc/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
