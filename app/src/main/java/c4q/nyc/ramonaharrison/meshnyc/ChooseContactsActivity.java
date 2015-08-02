ded06d15730b0f95855e13af17974ebc38527f5a
John Gomez <jgomez818@gmail.com>
2015-08-02 13:33:53 -0400

    added contacts listview and started app sharing sms

--- app/src/main/java/c4q/nyc/ramonaharrison/meshnyc/ChooseContactsActivity.java
+++ app/src/main/java/c4q/nyc/ramonaharrison/meshnyc/ChooseContactsActivity.java
@@ -1,59 +1,99 @@
 package c4q.nyc.ramonaharrison.meshnyc;
 
 import android.app.ListActivity;
+
 import android.database.Cursor;
+import android.graphics.Color;
 import android.provider.ContactsContract;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.os.Bundle;
+
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class ChooseContactsActivity extends ListActivity {
 
 
-        @Override
-        public long getSelectedItemId() {
-            // TODO Auto-generated method stub
-            return super.getSelectedItemId();
-        }
+    Button button;
+    ListView lv;
+    Cursor Cursor1;
+    ArrayList<String> phoneNumbers;
 
-        @Override
-        public int getSelectedItemPosition() {
-            // TODO Auto-generated method stub
-            return super.getSelectedItemPosition();
-        }
-        ListView lv;
-        Cursor Cursor1;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
 
-            //create a cursor to query the Contacts on the device to start populating a listview
-            Cursor1 = getContentResolver().query(
-                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                    null,
-                    null,
-                    null,
-                    null);
-            startManagingCursor(Cursor1);
+        //create a cursor to query the Contacts on the device to start populating a listview
+        Cursor1 = getContentResolver().query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null,
+                null,
+                null,
+                null);
+        startManagingCursor(Cursor1);
+        String[] from = {
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER};
+        int[] to = {R.id.display_name,
+                R.id.phone_number};
+        final SimpleCursorAdapter listadapter = new SimpleCursorAdapter(this, R.layout.contacts_fields, Cursor1, from, to);
+        setListAdapter(listadapter);
+        lv = getListView();
+        lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-            String[] from = {
-                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-                    ContactsContract.CommonDataKinds.Phone.NUMBER,
-                    ContactsContract.CommonDataKinds.Phone._ID}; // get the list items for the listadapter could be TITLE or URI
 
-            int[] to = {R.id.display_name,
-                    R.id.phone_number}; //sets the items from above string to listview
+                Cursor1.moveToPosition(i);
+                phoneNumbers = new ArrayList<String>();
+                String data = Cursor1.getString(Cursor1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                phoneNumbers.add(data);
+                view.setBackgroundColor(Color.BLUE);
+                Log.d("data", "" + data);
+            }
+        });
 
-            //new listadapter, created to use android checked template
-            SimpleCursorAdapter listadapter = new SimpleCursorAdapter(this, R.layout.contacts_fields, Cursor1, from, to );
-            setListAdapter(listadapter);
+        /**The following should be a button that when clicked sends a text message to all phone numbers stored in the
+        phone numbers array. But gives a null pointer exception when trying to compile
 
-            //adds listview so I can get data from it
-            lv = getListView();
-            lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        activity_chosen_contacts_result.xml was made to display the chosen contacts and then confirm that these
+        were the ones you wanted to send the text to, this was made along with SendAppInviteActivity
+         but has not been implemented yet. Those two are safe to delete if unnecessary.
 
-        }
+        activity_choose_contacts.xml is the the listview and the button reside. The fields populated by the simple
+        adapter are in  contacts_fields.xml*/
+
+
+//        button = (Button) findViewById(R.id.sendInviteButton);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+////                String itemText = data;
+//                // fetch the Sms Manager
+//                SmsManager sms = SmsManager.getDefault();
+//                // the message
+//                String messageText = "Hey ";
+//                // the phone numbers we want to send to
+////                String[] numbers = {itemText};
+//
+//                for (String contactNumber : phoneNumbers) {
+//                    sms.sendTextMessage(contactNumber, null, messageText, null, null);
+//                }
+//                Toast.makeText(getApplicationContext(), "Invite sent!", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
     }
+
+}
