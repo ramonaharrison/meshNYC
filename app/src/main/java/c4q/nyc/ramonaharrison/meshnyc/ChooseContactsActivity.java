d596b0a9b9030915840a10b4fcd213a9494d66e9
John Gomez <jgomez818@gmail.com>
2015-08-02 14:39:50 -0400

    finished sign up and contacts functions

--- app/src/main/java/c4q/nyc/ramonaharrison/meshnyc/ChooseContactsActivity.java
+++ app/src/main/java/c4q/nyc/ramonaharrison/meshnyc/ChooseContactsActivity.java
@@ -30,6 +30,7 @@ public class ChooseContactsActivity extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_choose_contacts);
 
 
         //create a cursor to query the Contacts on the device to start populating a listview
@@ -60,6 +61,24 @@ public class ChooseContactsActivity extends ListActivity {
                 phoneNumbers.add(data);
                 view.setBackgroundColor(Color.BLUE);
                 Log.d("data", "" + data);
+
+                String itemText = data;
+
+
+                // fetch the Sms Manager
+                SmsManager sms = SmsManager.getDefault();
+                // the message
+                String messageText = "Hey download our app";
+                // the phone numbers we want to send to
+               String[] numbers = {itemText};
+
+                for (String contactNumber : numbers) {
+                    sms.sendTextMessage(contactNumber, null, messageText, null, null);
+                }
+                Toast.makeText(getApplicationContext(), "Invite sent!", Toast.LENGTH_SHORT).show();
+
+
+
             }
         });
 
@@ -73,7 +92,7 @@ public class ChooseContactsActivity extends ListActivity {
         activity_choose_contacts.xml is the the listview and the button reside. The fields populated by the simple
         adapter are in  contacts_fields.xml*/
 
-
+//
 //        button = (Button) findViewById(R.id.sendInviteButton);
 //        button.setOnClickListener(new View.OnClickListener() {
 //            @Override
